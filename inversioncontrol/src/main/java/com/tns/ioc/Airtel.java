ddd4a03449c6a224ef12f2a6ddde18b4994c0370
Namra <Namra@192.168.1.206>
2022-04-03 12:20:07 +0530

    commit

---
+++ inversioncontrol/src/main/java/com/tns/ioc/Airtel.java
@@ -0,0 +1,17 @@
+package com.tns.ioc;
+
+public class Airtel implements Sim {
+
+
+		public void calling() {
+			System.out.println("Calling with Airtel Sim");
+			
+		}
+
+		public void data() {
+			System.out.println("Browsing with Airtel Sim");
+			
+		}
+
+	
+}
---
+++ inversioncontrol/src/main/java/com/tns/ioc/Jio.java
@@ -0,0 +1,17 @@
+package com.tns.ioc;
+
+public class Jio implements Sim {
+
+		public void calling() 
+		{
+			System.out.println("Calling with Jio Sim");
+
+		}
+
+		public void data()
+		{
+			System.out.println("Browsing with Jio Sim");
+		}
+
+	
+}
